0400e12774f184cf01ffb31bf59748905273c921
AmjadRathod <amjadrathod@gmail.com>
2018-11-29 13:11:59 +0530

    ue4t

--- js/main.js
+++ js/main.js
@@ -2,52 +2,22 @@
 $(document).ready(function(){
   Snipcart.execute('bind', 'cart.opened', function() {
     Snipcart.execute('unbind', 'cart.opened');
-    var user_login = $("#user-login").html();
-    $(user_login).insertBefore($("#snipcart-actions"));
-      });
-  Snipcart.execute('bind', 'cart.opened', function() {
-    Snipcart.execute('unbind', 'cart.opened');
     var html = $("#cart-content-text").html();
     $(html).insertBefore($("#snipcart-cartitems-continue-top"));
-  });
-  Snipcart.execute('bind', 'cart.opened', function() {
-    Snipcart.execute('unbind', 'cart.opened');
-    var user_pro = $("#user-profile-info").html();
-    $(user_pro).insertBefore($("#snipcart-header"));
-  });
-  Snipcart.execute('bind', 'cart.opened', function() {
-    Snipcart.execute('unbind', 'cart.opened');
-    var user_name = $("#user-profile-infoo").html();
-    $(user_name).insertBefore($("#snipcart-header"));
-  });
-  Snipcart.execute('bind', 'cart.opened', function() {
-    Snipcart.execute('unbind', 'cart.opened');
-    var continue_btn = $("#continue_btn").html();
-    $(continue_btn).insertBefore($("#snipcart-footer"));
-  });
-  Snipcart.execute('bind', 'cart.opened', function() {
-    Snipcart.execute('unbind', 'cart.opened');
+    var user_login = $("#user-login").html();
+    $(user_login).insertBefore($("#snipcart-actions"));
     var user_logout = $("#user-logout").html();
     $(user_logout).insertAfter($("#snipcart-current-user"));
-  });
-  Snipcart.execute('bind', 'cart.opened', function() {
-    Snipcart.execute('unbind', 'cart.opened');
+    var user_name = $("#user-profile-infoo").html();
+    $(user_name).insertBefore($("#snipcart-header"));
+    var continue_btn = $("#continue_btn").html();
+    $(continue_btn).insertBefore($("#snipcart-footer"));
     var site_logo = $("#site-logo").html();
     $(site_logo).insertBefore($("#snipcart-title"));
-  });
-  Snipcart.execute('bind', 'cart.opened', function() {
-    Snipcart.execute('unbind', 'cart.opened');
+    var user_pro = $("#user-profile-info").html();
+    $(user_pro).insertBefore($("#snipcart-header"));
     var add_order = $("#add_order").html();
     $(add_order).insertAfter($("#snipcart-actions"));
-  });
-  $(".snipcart-user-profile").click(function(){
-    $(this).css({"display": "none"});
-    $('.logout-user').css("display": "block");
-  });
-  $('.logout-user').css("display": "none");
-  $(".logout-user").click(function(){
-    $(this).css("display": "none");
-    $('.snipcart-user-profile').css("display": "block");
   });
   Snipcart.subscribe('page.changed', function (page) {
     if (page == 'order-confirm') {
