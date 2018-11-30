2b9dbc5c53af05ede3158189cff43c96806803ab
AmjadRathod <amjadrathod@gmail.com>
2018-11-30 18:09:21 +0530

    loguot btn

--- js/main.js
+++ js/main.js
@@ -131,7 +131,7 @@ $(document).ready(function(){
     $("#snipcart-header").append('<p class="sub-title">YOUR CART for approval</p>');
     if (page == 'cart-content') {
       $("#snipcart-discounts .snip-actions").append('<div id="snipcart-current-user-login"><div class="snip-header__user signup"><a href="#" class="snipcart-user-profile"><span class="snipcart-user-email">I&#39;M MEMBER</span></a></div></div>');
-      $(".snip-header__user-text").append('<a href="#" class="snipcart-user-logout">(logout/change)</a>');
+      $("#snipcart-header").append('<a href="#" class="snipcart-user-logout">(logout/change)</a>');
       $("#snipcart-actions").append('<a id="snipcart-cartitems-continue-top" class="snip-btn snip-header__continue">&lt;&lt; Add another Order</a>');
       $("#snipcart-items-list").last().append('<div id="snipcart-header-total" class="snip-header__total"><span class="snip-header__total-label">ORDER TOTAL:</span><span id="snipcart-amount"></span></div>');
       $('#snipcart-actions .js-next').text('DELIVERY >>');
