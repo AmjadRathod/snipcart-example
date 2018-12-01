988c5946b76b03b0f2c34dc5874e300ffc390f47
AmjadRathod <amjadrathod@gmail.com>
2018-12-01 11:21:48 +0530

    lodout btn many time

--- js/main.js
+++ js/main.js
@@ -146,6 +146,7 @@ $(document).ready(function(){
     if (page == 'cart-content') {
       // Snipcart.settings.onlyAllowGuests = false;
       $("#snipcart-discounts .snip-actions").append('<div id="snipcart-current-user-login"><div class="snip-header__user signup"><a href="#" class="snipcart-user-profile"><span class="snipcart-user-email">I&#39;M MEMBER</span></a></div></div>');
+      $('.snipcart-user-logout').remove();
       $("#snipcart-current-user").append('<a href="#" class="snipcart-user-logout">(logout/change)</a>');
       $("#snipcart-actions").append('<a id="snipcart-cartitems-continue-top" class="snip-btn snip-header__continue">&lt;&lt; Add another Order</a>');
       $("#snipcart-items-list").last().append('<div id="snipcart-header-total" class="snip-header__total"><span class="snip-header__total-label">ORDER TOTAL:</span><span id="snipcart-amount"></span></div>');
