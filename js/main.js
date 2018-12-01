c2b3ac832f00c6e454849c24b3a93a699b9ca8c9
AmjadRathod <amjadrathod@gmail.com>
2018-12-01 15:39:20 +0530

    custom login

--- js/main.js
+++ js/main.js
@@ -251,18 +251,22 @@ $(document).ready(function(){
     }
       console.log(page);
   });
+  // $('.accordion').on
+  $( ".accordion" ).click(function() {
+    $('#snipcart-login-form-container').toggle();
+  });
 });
-var acc = document.getElementsByClassName("accordion");
-var i;
-for (i = 0; i < acc.length; i++) {
-    acc[i].addEventListener("click", function() {
-        this.classList.toggle("active");
-        var panel = this.nextElementSibling;
-        if (panel.style.display === "block") {
-            panel.style.display = "none";
-        } else {
-            panel.style.display = "block";
-        }
-    });
-}
-console.log(acc);
+// var acc = document.getElementsByClassName("accordion");
+// var i;
+// for (i = 0; i < acc.length; i++) {
+//     acc[i].addEventListener("click", function() {
+//         this.classList.toggle("active");
+//         var panel = this.nextElementSibling;
+//         if (panel.style.display === "block") {
+//             panel.style.display = "none";
+//         } else {
+//             panel.style.display = "block";
+//         }
+//     });
+// }
+// console.log(acc);
