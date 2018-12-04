4c4d2b55b68042e3595b35c21407e272381ae3f0
AmjadRathod <amjadrathod@gmail.com>
2018-12-04 12:40:42 +0530

    alignment of address fields TESTING

--- js/main.js
+++ js/main.js
@@ -87,8 +87,8 @@ $(document).ready(function(){
     if (page == 'login') {
       $('.sub-title').text(' ');
     }
-    if (page == 'billing-address') {
-
+    if (page == 'billing-address') {    
+      $('div [data-for="shippingSameAsBilling"]').insertBefore('div [data-for="name"]');
       $('#snipcart-billingaddress-form .snipcart-checkbox-field label').text('DELIVERY ADDRESS same as INVOICE ADDRESS');
       $('#snipcart-previous').text('<< Go Back');
       $('#snipcart-previous').css({"background": "brown", "color": "yellow"});
