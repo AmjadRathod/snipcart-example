149ddb13bf61ca79d2cbe91fe1b25234a7d06097
Amjadrathod <amjadrathod@gmail.com>
2018-12-15 17:58:12 +0530

    added latest css and js file

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 Snipcart.subscribe('cart.opened', function() {
   Snipcart.appView.setTitle('');
-  $('#snipcart-title').append('<img src="/snipcart-example/img/logo2.jpeg" alt="Reliaprint.co.uk">');
+  $('#snipcart-title').append('<img src="https://reliaprint.co.uk/ajayx/img/logo2X.jpeg" alt="Reliaprint.co.uk">');
   var user = Snipcart.api.user.current();
   if (user) {
     $('.snip-header__user-text').text("Signed-in as : " + user['email'])
@@ -10,8 +10,12 @@ Snipcart.subscribe('cart.opened', function() {
   }
   var email_width = $('.snip-header__user-text').width();
   $('.snipcart-user-logout').css({
-    "left": email_width + 50
+    "left": email_width + 40
   });
+  if (window.matchMedia('(max-width: 767px)').matches) {
+        $('.snipcart-user-logout').css({
+        "left": email_width + 32 });
+      }
 });
 
 $(document).ready(function() {
@@ -25,7 +29,9 @@ $(document).ready(function() {
     $('#snipcart-show-discount-box').attr('id', 'newId');
   }
   Snipcart.subscribe('item.removed', function (item) {
-  if (Snipcart.cartIsEmpty()) {
+ var total = $('#snipcart-amount').text();
+$('#snipcart-items-list #snipcart-header-total #snipcart-amount').text(total);
+if (Snipcart.cartIsEmpty()) {
       $('.sub-title').remove();
       $('.sub-title').css({"display":"none", "visiblity":"hidden"});
     }
@@ -48,7 +54,7 @@ $(document).ready(function() {
     }
     Snipcart.appView.setTitle('');
     $('#snipcart-title img').remove();
-    $('#snipcart-title').append('<img src="/snipcart/img/logo2.jpeg" alt="Reliaprint.co.uk">');
+    $('#snipcart-title').append('<img src="https://reliaprint.co.uk/ajayx/img/logo2X.jpeg" alt="Reliaprint.co.uk">');
     if (page == 'customer/orders-history') {
       var curUrl = window.location.href;
       var dir = '#!/';
@@ -63,23 +69,19 @@ $(document).ready(function() {
       // Snipcart.settings.onlyAllowGuests = false;
       $('#snipcart-items-header tr th:nth-child(5)').text('PRICE');
       $("#snipcart-discounts .snip-actions").append('<div id="snipcart-current-user"><div class="snip-header__user signup"><a href="#" class="snipcart-user-profile"><span class="snipcart-user-email">I&#39;M A MEMBER</span></a></div></div>');
-      $('.js-next.snip-btn.snip-btn--highlight.snip-btn--right').prop("class","js-next-invoice snip-btn snip-btn--highlight snip-btn--right");
-      $(".js-next-invoice").click(function() {
-        var curUrl = window.location.href;
-        var dir = '#!/';
-        var url = curUrl.split(dir)[0];
-        window.location.replace(url + dir + 'shipping-address');
-      });
 
       $('.snipcart-user-logout').remove();
-      $("#snipcart-current-user").append('<a href="#" class="snipcart-user-logout">(logout/change)</a>');
+      $("#snipcart-current-user").append('<a href="#" class="snipcart-user-logout">(logout & empty cart)</a>');
       $("#snipcart-actions").append('<a id="snipcart-cartitems-continue-top" class="snip-btn snip-header__continue">&lt;&lt; Add another Order</a>');
       var email_width = $('.snip-header__user-text').width();
       $('.snipcart-user-logout').css({
-        "left": email_width + 60 });
-
+        "left": email_width + 40 });
+      if (window.matchMedia('(max-width: 767px)').matches) {
+        $('.snipcart-user-logout').css({
+        "left": email_width + 32 });
+      }
       $("#snipcart-items-list").last().append('<div id="snipcart-header-total" class="snip-header__total"><span class="snip-header__total-label">ORDER TOTAL:</span><span id="snipcart-amount"></span></div>');
-      $('#snipcart-actions .js-next').text('DELIVERY >>');
+      $('#snipcart-actions .js-next').text("Let's Go >>");
       var total = $('#snipcart-amount').text();
       $('#snipcart-items-list #snipcart-header-total #snipcart-amount').text(total);
       var user = Snipcart.api.user.current();
@@ -114,17 +116,42 @@ $(document).ready(function() {
       $('.sub-title').text(' ');
     }
     if (page == 'billing-address') {
-      $('div [data-for="shippingSameAsBilling"]').insertBefore('div [data-for="name"]');
-      $('div [data-for="country"]').insertAfter('div [data-for="address2"]');
-      $('div [data-for="postalCode"]').insertAfter('div [data-for="city"]');
-      $('div [data-for="province"]').insertAfter('div [data-for="email"]');
+        $("input[type='text']").attr('value','');
+        $(".snipcart-user-logout").css({
+          "visibility": "hidden"
+        });
+        var email_width = $('.snip-header__user-text').width();
+          $('.snipcart-user-logout').css({
+            "left": email_width
+          });
+      $('#snipcart-next').prop('id','snipcart-next-submitt');
+      $('#snip-name').prop('required',true);
+      $('#snip-address1').prop('required',true);
+      $('#snip-city').prop('required',true);
+      $('#snip-postalCode').prop('required',true);
+      $('#snip-email').prop('type', 'email');
+      $('#snip-email').prop('required',true);
+      $('#snipcart-next-submitt').click(function(){
+        $("#snipcart-billingaddress-form .snip-actions input[type='submit']").trigger("click");
+      });
+      $('<span class="required-filed-lable">  [REQUIRED]</span>').insertAfter('#snipcart-billingaddress-form div [data-for="name"] label');
+      $('<span class="required-filed-lable">  [REQUIRED]</span>').insertAfter('#snipcart-billingaddress-form div [data-for="email"] label');
+      $('<span class="required-filed-lable">  [REQUIRED]</span>').insertAfter('#snipcart-billingaddress-form div [data-for="address1"] label');
+      $('<span class="required-filed-lable">  [REQUIRED]</span>').insertAfter('#snipcart-billingaddress-form div [data-for="city"] label');
+      $('<span class="required-filed-lable">  [REQUIRED]</span>').insertAfter('#snipcart-billingaddress-form div [data-for="postalCode"] label');
+
+      $('div [data-for="shippingSameAsBilling"]').insertBefore('div [data-for="province"]');
+      $('div [data-for="country"]').insertAfter('div [data-for="city"]');
+      $('div [data-for="postalCode"]').insertAfter('div [data-for="country"]');
+      $('div [data-for="province"]').insertAfter('div [data-for="phone"]');
+      $('div [data-for="email"]').insertAfter('div [data-for="name"]');
       $('#snipcart-billingaddress-form .snipcart-checkbox-field label').text('DELIVERY ADDRESS same as INVOICE ADDRESS');
       $('#snipcart-previous').text('<< Go Back');
       $('#snipcart-previous').css({
         "background": "brown",
         "color": "yellow"
       });
-      $('#snipcart-next').text('Continue >>');
+      $('#snipcart-next-submitt').text('Continue >>');
       $('#snipcart-next').css({
         "background": "blue",
         "color": "yellow"
@@ -137,21 +164,21 @@ $(document).ready(function() {
       //remove
       $('#snipcart-billingaddress-form div [data-for="province"] input').remove();
       $('#snipcart-billingaddress-form div [data-for="province"]').append('<textarea rows="6" cols="50" id="snip-province" name="province" value="" class="snipcart-state"></textarea>');
-
       $('#snipcart-billingaddress-form div [data-for="country"] select').remove();
       $('#snipcart-billingaddress-form div [data-for="country"]').append('<input type="text" id="snip-country" name="country" class="snipcart-country">');
+
       // lable text
-      $("#snipcart-billingaddress-form label[for='snip-name']").text('YOUR NAME :');
-      $("#snipcart-billingaddress-form label[for='snip-company']").text('COMPANY or ORGANISATION NAME :');
+      $("#snipcart-billingaddress-form label[for='snip-name']").text('YOUR NAME :  ');
+      $("#snipcart-billingaddress-form label[for='snip-company']").text('COMPANY or ORGANISATION NAME :  ');
       $("#snipcart-billingaddress-form label[for='snip-phone']").text('YOUR PHONE NUMBER (optional) :');
-      $("#snipcart-billingaddress-form label[for='snip-address1']").text('ADDRESS LINE 1* :');
-      $("#snipcart-billingaddress-form label[for='snip-address2']").text('ADDRESS LINE 2 :');
+      $("#snipcart-billingaddress-form label[for='snip-address1']").text('ADDRESS LINE 1 :  ');
+      $("#snipcart-billingaddress-form label[for='snip-address2']").text('ADDRESS LINE 2 :  ');
       $("#snipcart-billingaddress-form label[for='snip-country']").text('COUNTY (optional) :');
-      $("#snipcart-billingaddress-form label[for='snip-city']").text('TOWN CITY* :');
-      $("#snipcart-billingaddress-form label[for='snip-postalCode']").text('POSTCODE* :');
-      $("#snipcart-billingaddress-form label[for='snip-email']").text('YOUR EMAIL ADDRESS* :');
+      $("#snipcart-billingaddress-form label[for='snip-city']").text('TOWN / CITY :  ');
+      $("#snipcart-billingaddress-form label[for='snip-postalCode']").text('POSTCODE :  ');
+      $("#snipcart-billingaddress-form label[for='snip-email']").text('YOUR EMAIL ADDRESS :  ');
       $("#snipcart-billingaddress-form label[for='snip-province']").text('ANY SPECIAL DELIVERY INSTRUCTIONS :');
-      $("#snipcart-next").hover(function(){
+      $("#snipcart-next-submitt").hover(function(){
         txtName = $("#snipcart-billingaddress-form #snip-name").val();
         txtCompany = $("#snipcart-billingaddress-form #snip-company").val();
         txtAddress1 = $("#snipcart-billingaddress-form #snip-address1").val();
@@ -175,14 +202,18 @@ $(document).ready(function() {
     }
 
     if (page == 'shipping-address') {
-      $('div [data-for="phone"]').insertAfter('div [data-for="company"]');
+    //   $("input[type='text']").attr('value','');
+      $('div [data-for="phone"]').insertAfter('div [data-for="name"]');
       $('div [data-for="province"]').remove();
       $('<div data-for="shippingSameAsBilling" class="snip-form__container snip-form__container--checkbox snipcart-checkbox-field"><input type="checkbox" name="shippingSameAsBilling" id="snip-shippingSameAsBilling" class="snip-product__customfields-checkbox"><label for="snip-shippingSameAsBilling" class="snip-form__label">INVOICE ADDRESS same as DELIVERY ADDRESS</label></div>').insertBefore('div [data-for="name"]');
 
       $('#snipcart-shipping-address-form div [data-for="country"] select').remove();
       $('#snipcart-shipping-address-form div [data-for="country"]').append('<input type="text" id="snip-country" name="country" class="snipcart-country">');
-      // lable
-      $('div [data-for="country"]').insertAfter('div [data-for="address2"]');
+      $('<span class="required-filed-lable">  [REQUIRED]</span>').insertAfter('#snipcart-shipping-address-form div [data-for="name"] label');
+      $('<span class="required-filed-lable">  [REQUIRED]</span>').insertAfter('#snipcart-shipping-address-form div [data-for="phone"] label');
+
+
+    //   $('div [data-for="country"]').insertAfter('div [data-for="address2"]');
       $('#snipcart-previous').text('<< Go Back');
       $('#snipcart-previous').css({
         "background": "brown",
@@ -197,16 +228,23 @@ $(document).ready(function() {
         "background": "blue",
         "color": "yellow"
       });
+      $('#snipcart-next').prop('id','snipcart-next-submitt');
+      $('#snip-name').prop('required',true);
+      $('#snip-phone').prop('type', 'email');
+      $('#snip-phone').prop('required',true);
+      $('#snipcart-next-submitt').click(function(){
+        $("#snipcart-shipping-address-form .snip-actions input[type='submit']").trigger("click");
+      });
       $('.sub-title').text('Invoice Address');
       // lable text
-      $("#snipcart-shipping-address-form label[for='snip-name']").text('INVOICE TO :');
+      $("#snipcart-shipping-address-form label[for='snip-name']").text('INVOICE TO :  ');
       $("#snipcart-shipping-address-form label[for='snip-company']").text('COMPANY or ORGANISATION NAME (optional) :');
-      $("#snipcart-shipping-address-form label[for='snip-address1']").text('INVOICE ADDRESS LINE 1* :');
+      $("#snipcart-shipping-address-form label[for='snip-address1']").text('INVOICE ADDRESS LINE 1 :');
       $("#snipcart-shipping-address-form label[for='snip-address2']").text('INVOICE ADDRESS LINE 2 :');
       $("#snipcart-shipping-address-form label[for='snip-country']").text('COUNTY (optional) :');
-      $("#snipcart-shipping-address-form label[for='snip-city']").text('TOWN CITY* :');
-      $("#snipcart-shipping-address-form label[for='snip-postalCode']").text('INVOICE POSTCODE* :');
-      $('#snip-layout-shipping-address label[for="snip-phone"]').text("INVOICE EMAIL ADDRESS *");
+      $("#snipcart-shipping-address-form label[for='snip-city']").text('TOWN CITY :');
+      $("#snipcart-shipping-address-form label[for='snip-postalCode']").text('INVOICE POSTCODE :');
+      $('#snip-layout-shipping-address label[for="snip-phone"]').text("INVOICE EMAIL ADDRESS :  ");
       $('#snip-layout-shipping-address #snip-shippingSameAsBilling').click(function() {
         if ($(this).prop('checked') == true) {
           $("#snipcart-shipping-address-form #snip-name").val(txtName);
@@ -216,20 +254,28 @@ $(document).ready(function() {
           $("#snipcart-shipping-address-form #snip-phone").val(txtEmail);
           $("#snipcart-shipping-address-form #snip-city").val(txtCity);
           $("#snipcart-shipping-address-form #snip-postalCode").val(txtPostCode);
-          $("#snipcart-next").trigger("click");
+          $("#snipcart-next-submitt").trigger("click");
         }
       });
     }
 
     if (page == 'payment-method') {
+        // $("input[type='text']").attr('value','');
+      $('#snipcart-paymentmethod-pay').prop('id','snipcart-paymentmethod-payy');
+      $('#snip-ownerName').prop('required',true);
+      $('#snip-number').prop('required',true);
+      $('#snip-cvc').prop('required',true);
+      $('#snipcart-paymentmethod-payy').click(function(){
+        $("#snip-layout-payment-method .snip-actions input[type='submit']").trigger("click");
+      });
       $('#snipcart-previous').text('<< Go Back');
       $('#snipcart-previous').css({
         "background": "brown",
         "color": "yellow"
       });
-      $('#snipcart-paymentmethod-pay').text('PLACE ORDER');
-      $('#snipcart-paymentmethod-pay').css({
-        "background": "#F27746",
+      $('#snipcart-paymentmethod-payy').text('Continue >>');
+      $('#snipcart-paymentmethod-payy').css({
+        "background": "blue",
         "color": "yellow"
       });
       $('.sub-title').text('Payment Method');
@@ -245,9 +291,11 @@ $(document).ready(function() {
       $("#snipcart-next").css({
         "background": "blue",
       });
+      $('.sub-title').text('Shipping Method');
     }
     if (page == 'login') {
-      $("#snipcart-login-form-container form").append('<a href="#" id="snipcart-login-cancel" class="snip-btn snip-btn--full">Cancel</a>');
+    $('#snipcart-login-forgotpassword-link').text("I forgot my password (or would like to change it)");
+  $("#snipcart-login-form-container form").append('<a href="#" id="snipcart-login-cancel" class="snip-btn snip-btn--full">Cancel</a>');
       $("#snipcart-login-cancel").click(function() {
         var curUrl = window.location.href;
         var dir = '#!/';
@@ -256,10 +304,20 @@ $(document).ready(function() {
       });
     }
     if (page == 'order-confirm') {
-      $(".js-submit").trigger("click");
+        $('.sub-title').css("padding-top","70px");
+        $('.sub-title').text('Confirm Order');
+      $('#snip-layout-order-confirm .js-submit').css({
+        "background": "#F27746",
+        "color": "yellow"
+      });
+      $('#snip-layout-order-confirm .js-back').text('<< Go Back');
+      $('#snip-layout-order-confirm .js-back').css({
+        "background": "brown",
+        "color": "yellow"
+      });
     }
     if (page == 'order-details') {
-      window.location.replace('/snipcart-example/sucess/');
+      window.location.replace('../success/');
     }
     if (page == 'empty-cart') {
       Snipcart.appView.close();
