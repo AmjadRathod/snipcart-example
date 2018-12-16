0623664c3ae8c3168548ee97d67a5e6bb17ca830
Amjadrathod <amjadrathod@gmail.com>
2018-12-16 16:42:57 +0530

    jason

--- js/main.js
+++ js/main.js
@@ -328,7 +328,7 @@ if (Snipcart.cartIsEmpty()) {
     }
   });
 });
-// Snipcart.execute('bind', 'order.completed', function (order) {
-// var url = '/snipcart-example/sucess/?order=' + order.token;
-// window.location.href = url;
-// });
+Snipcart.execute('bind', 'order.completed', function (order) {
+var url = '/snipcart-example/sucess/?order=' + order.token;
+window.location.href = url;
+});
